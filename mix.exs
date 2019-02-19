6af9e68387f84fbc2804645508b46c4defc15f88
Brian Balser <brian.balser@gmail.com>
2019-02-19 11:15:31 -0500

    Updated version to 1.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Placebo.MixProject do
   def project do
     [
       app: :placebo,
-      version: "1.2.0",
+      version: "1.2.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: description(),
