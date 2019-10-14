6cdf0623b34770620dfecf1e8acaaed40062ccf9
Brian Balser <bbalser@brians-mbp.attlocal.net>
2019-10-14 09:43:46 -0400

    Bumped version to 2.0.0-rc.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Placebo.MixProject do
   def project do
     [
       app: :placebo,
-      version: "1.2.2",
+      version: "2.0.0-rc.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: description(),
