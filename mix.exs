89fe63b5d9e0f25a16282d075eaadfd44b315d1c
Folkerth, Jennifer <jennifer.folkerth@accenture.com>
2022-07-28 17:46:09 -0400

    Preserving backwards compatibility

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Placebo.MixProject do
     [
       app: :placebo,
       version: "2.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
